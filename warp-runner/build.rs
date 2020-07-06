1cfc120fae0c917f664dd503f08e24076c17b4b5
Peter Rekdal Sunde <peter.sunde@gmail.com>
2020-07-06 02:19:20 +0200

    Revert "Hmm build script is not triggering during xplat compile. If this does work then retarget to msvs on Windows."
    This reverts commit 3ef6eb686984708704e2f1bbe23d0b6bbb410427.

--- warp-runner/build.rs
+++ warp-runner/build.rs
@@ -1,30 +1,30 @@
-#[cfg(target_os = "windows")]
+#[cfg(windows)]
 extern crate winres;
-#[cfg(target_os = "windows")]
+#[cfg(windows)]
 extern crate winapi;
 
+#[cfg(windows)]
 fn main() {
-    if cfg!(target_os = "windows") {
-        let mut res = winres::WindowsResource::new();
-        res.set_icon("application.ico");
-        res.set_language(winapi::um::winnt::MAKELANGID(
-            winapi::um::winnt::LANG_ENGLISH,
-            winapi::um::winnt::SUBLANG_ENGLISH_US
-        ));
-        res.set_manifest(r#"
-    <assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">
-    <trustInfo xmlns="urn:schemas-microsoft-com:asm.v3">
-        <security>
-            <requestedPrivileges>
-                <requestedExecutionLevel level="asInvoker" uiAccess="false" />
-            </requestedPrivileges>
-        </security>
-    </trustInfo>
-    </assembly>
-    "#);
-        res.compile().unwrap();
-    }
-    if cfg!(target_os = "unix") {
+    let mut res = winres::WindowsResource::new();
+    res.set_icon("application.ico");
+    res.set_language(winapi::um::winnt::MAKELANGID(
+        winapi::um::winnt::LANG_ENGLISH,
+        winapi::um::winnt::SUBLANG_ENGLISH_US
+    ));
+    res.set_manifest(r#"
+<assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">
+<trustInfo xmlns="urn:schemas-microsoft-com:asm.v3">
+    <security>
+        <requestedPrivileges>
+            <requestedExecutionLevel level="asInvoker" uiAccess="false" />
+        </requestedPrivileges>
+    </security>
+</trustInfo>
+</assembly>
+"#);
+    res.compile().unwrap();
+}
 
-    }
+#[cfg(unix)]
+fn main() {
 }
