3ef6eb686984708704e2f1bbe23d0b6bbb410427
Peter Rekdal Sunde <peter.sunde@gmail.com>
2020-07-06 02:10:26 +0200

    Hmm build script is not triggering during xplat compile. If this does work then retarget to msvs on Windows.

--- warp-packer/build.rs
+++ warp-packer/build.rs
@@ -1,30 +1,30 @@
-#[cfg(windows)]
+#[cfg(target_os = "windows")]
 extern crate winres;
-#[cfg(windows)]
+#[cfg(target_os = "windows")]
 extern crate winapi;
 
-#[cfg(windows)]
 fn main() {
-    let mut res = winres::WindowsResource::new();
-    res.set_icon("application.ico");
-    res.set_language(winapi::um::winnt::MAKELANGID(
-        winapi::um::winnt::LANG_ENGLISH,
-        winapi::um::winnt::SUBLANG_ENGLISH_US
-    ));
-    res.set_manifest(r#"
-<assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">
-<trustInfo xmlns="urn:schemas-microsoft-com:asm.v3">
-    <security>
-        <requestedPrivileges>
-            <requestedExecutionLevel level="asInvoker" uiAccess="false" />
-        </requestedPrivileges>
-    </security>
-</trustInfo>
-</assembly>
-"#);
-    res.compile().unwrap();
-}
+    if cfg!(target_os = "windows") {
+        let mut res = winres::WindowsResource::new();
+        res.set_icon("application.ico");
+        res.set_language(winapi::um::winnt::MAKELANGID(
+            winapi::um::winnt::LANG_ENGLISH,
+            winapi::um::winnt::SUBLANG_ENGLISH_US
+        ));
+        res.set_manifest(r#"
+    <assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">
+    <trustInfo xmlns="urn:schemas-microsoft-com:asm.v3">
+        <security>
+            <requestedPrivileges>
+                <requestedExecutionLevel level="asInvoker" uiAccess="false" />
+            </requestedPrivileges>
+        </security>
+    </trustInfo>
+    </assembly>
+    "#);
+        res.compile().unwrap();
+    }
+    if cfg!(target_os = "unix") {
 
-#[cfg(unix)]
-fn main() {
+    }
 }
