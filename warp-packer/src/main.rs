cc8d95026e0ae4a09ea21b7631252edb08ad5fc8
Peter Rekdal Sunde <peter.sunde@gmail.com>
2020-07-06 02:28:17 +0200

    Update paths.

--- warp-packer/src/main.rs
+++ warp-packer/src/main.rs
@@ -29,8 +29,8 @@ const RUNNER_MAGIC: &[u8] = b"tVQhhsFFlGGD3oWV4lEPST8I8FEPP54IM0q7daes4E1y3p2U2w
 
 const RUNNER_LINUX_X64: &[u8] = include_bytes!("../../target/x86_64-unknown-linux-gnu/release/warp-runner");
 const RUNNER_MACOS_X64: &[u8] = include_bytes!("../../target/x86_64-apple-darwin/release/warp-runner");
-const RUNNER_WINDOWS_X86: &[u8] = include_bytes!("../../target/i686-pc-windows-gnu/release/warp-runner.exe");
-const RUNNER_WINDOWS_X64: &[u8] = include_bytes!("../../target/x86_64-pc-windows-gnu/release/warp-runner.exe");
+const RUNNER_WINDOWS_X86: &[u8] = include_bytes!("../../target/i686-pc-windows-msvc/release/warp-runner.exe");
+const RUNNER_WINDOWS_X64: &[u8] = include_bytes!("../../target/x86_64-pc-windows-msvc/release/warp-runner.exe");
 
 lazy_static! {
     static ref RUNNER_BY_ARCH: HashMap<&'static str, &'static [u8]> = {
