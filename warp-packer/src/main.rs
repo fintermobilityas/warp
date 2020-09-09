0f3f1590b105c8a4f496e7e3ff74394cc686ebb3
Peter Rekdal Sunde <peter.sunde@gmail.com>
2020-09-10 00:04:29 +0200

    Add RUNNER_LINUX_AARCH64

--- warp-packer/src/main.rs
+++ warp-packer/src/main.rs
@@ -28,6 +28,7 @@ const VERSION: &str = env!("CARGO_PKG_VERSION");
 const RUNNER_MAGIC: &[u8] = b"tVQhhsFFlGGD3oWV4lEPST8I8FEPP54IM0q7daes4E1y3p2U2wlJRYmWmjPYfkhZ0PlT14Ls0j8fdDkoj33f2BlRJavLj3mWGibJsGt5uLAtrCDtvxikZ8UX2mQDCrgE\0";
 
 const RUNNER_LINUX_X64: &[u8] = include_bytes!("../../target/x86_64-unknown-linux-gnu/release/warp-runner");
+const RUNNER_LINUX_AARCH64: &[u8] = include_bytes!("../../target/aarch64-unknown-linux-gnu/release/warp-runner");
 const RUNNER_MACOS_X64: &[u8] = include_bytes!("../../target/x86_64-apple-darwin/release/warp-runner");
 const RUNNER_WINDOWS_X86: &[u8] = include_bytes!("../../target/i686-pc-windows-msvc/release/warp-runner.exe");
 const RUNNER_WINDOWS_X64: &[u8] = include_bytes!("../../target/x86_64-pc-windows-msvc/release/warp-runner.exe");
@@ -36,6 +37,7 @@ lazy_static! {
     static ref RUNNER_BY_ARCH: HashMap<&'static str, &'static [u8]> = {
         let mut m = HashMap::new();
         m.insert("linux-x64", RUNNER_LINUX_X64);
+        m.insert("linux-aarch64", RUNNER_LINUX_AARCH64);
         m.insert("macos-x64", RUNNER_MACOS_X64);
         m.insert("windows-x86", RUNNER_WINDOWS_X86);
         m.insert("windows-x64", RUNNER_WINDOWS_X64);
